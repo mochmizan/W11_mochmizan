4581ac6237a791c260b74c49766d176960a640f0
Mizan <ghodafail@gmail.com>
2025-05-22 00:45:51 +0700

    Update Soal_3.py

--- Soal_3.py
+++ Soal_3.py
@@ -1,52 +1,47 @@
 #Soal 3
-from tkinter import Tk, Label, Button, Entry, IntVar, END, W, E
-
-class Calculator:
-    def __init__(self, master):
-        self.master = master
-        master.title("Calculator")
-        self.total = 0
-        self.entered_number = 0
-        self.total_label_text = IntVar()
-        self.total_label_text.set(self.total)
-        self.total_label = Label(master, textvariable=self.total_label_text)
-        self.label = Label(master, text="Total:")
-        vcmd = master.register(self.validate) # we have to wrap the command
-        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
-        self.add_button = Button(master, text="+",
-                                 command=lambda: self.update("add"))
-        self.subtract_button = Button(master, text="-",
-                                      command=lambda: self.update("subtract"))
-        self.reset_button = Button(master, text="Reset",
-                                   command=lambda: self.update("reset"))
-        # LAYOUT
-        self.label.grid(row=0, column=0, sticky=W)
-        self.total_label.grid(row=0, column=1, columnspan=2, sticky=E)
-        self.entry.grid(row=1, column=0, columnspan=3, sticky=W+E)
-        self.add_button.grid(row=2, column=0)
-        self.subtract_button.grid(row=2, column=1)
-        self.reset_button.grid(row=2, column=2, sticky=W+E)
-
-    def validate(self, new_text):
-        if not new_text: # the field is being cleared
-            self.entered_number = 0
-            return True
-        try:
-            self.entered_number = int(new_text)
-            return True
-        except ValueError:
-            return False
-
-    def update(self, method):
-        if method == "add":
-            self.total += self.entered_number
-        elif method == "subtract":
-            self.total -= self.entered_number
-        else: # reset
-            self.total = 0
-        self.total_label_text.set(self.total)
-        self.entry.delete(0, END)
+from tkinter import *
 
 root = Tk()
-my_gui = Calculator(root)
-root.mainloop()
+
+# Judul dan Ukuran
+root.title("Aplikasi Paint")
+root.geometry("500x500")
+
+# Fungsi mewarna
+def paint(event):
+    # Koordinat mouse
+    x1, y1, x2, y2 = (event.x - 2), (event.y - 2), (event.x + 2), (event.y + 2)
+    # Warna
+    Colour = "#000000"
+    # Membuat garis
+    w.create_line(x1, y1, x2, y2, fill=Colour)
+
+drawing_frame = Frame(root)
+drawing_frame.grid(row=0, column=0, pady=10, padx=10, sticky="ew")
+root.grid_columnconfigure(0, weight=1)
+
+# Label
+instruction_label = Label(drawing_frame, text="Klik dan Drag untuk Menggambar")
+instruction_label.pack()
+
+# Canvas
+w = Canvas(drawing_frame, width=400, height=250, highlightthickness=1,
+           highlightbackground="black", bg="white")
+w.pack(pady=5)
+w.bind("<B1-Motion>", paint)
+
+# fungsi hapus entry
+def hapus():
+    entry.delete(0, END)
+
+# ---  Text, Entry, dan Button ---
+jlabel = Label(root, text="Nama:")
+jlabel.grid(row=2, column=0, sticky="w", padx=45, pady=(0, 10))
+nama_text = Text(root, height = 1, width = 6)
+nama_text.grid(row=2, column=0, padx=0, pady=(0, 10))
+reset_button = Button(root, text="Reset Entry", command=hapus)
+reset_button.grid(row=3, column=0, sticky="w", padx=45, pady=(0, 10))
+entry = Entry(root, width=30)
+entry.grid(row=3, column=0, padx=45, pady=(0, 10))
+
+mainloop()
