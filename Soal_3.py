3d4d0f95688c43a935a377f58c41045cfde030dd
Mizan <ghodafail@gmail.com>
2025-05-22 00:46:27 +0700

    Update Soal_3.py

--- Soal_3.py
+++ Soal_3.py
@@ -1,47 +1,47 @@
 #Soal 3
-from tkinter import *
+class Model(object):
+  services = {
+      'email': {'number': 1000, 'price': 2,},
+      'sms': {'number': 1000, 'price': 10,},
+      'voice': {'number': 1000, 'price': 15,},
+  }
 
-root = Tk()
+class View(object):
+  def list_services(self, services):
+    for svc in services:
+      print(svc,'')
 
-# Judul dan Ukuran
-root.title("Aplikasi Paint")
-root.geometry("500x500")
+  def list_pricing(self, services):
+    for svc in services:
+      print("For", Model.services[svc]['number'],
+            svc, "message you pay $",
+            Model.services[svc]['price'])
 
-# Fungsi mewarna
-def paint(event):
-    # Koordinat mouse
-    x1, y1, x2, y2 = (event.x - 2), (event.y - 2), (event.x + 2), (event.y + 2)
-    # Warna
-    Colour = "#000000"
-    # Membuat garis
-    w.create_line(x1, y1, x2, y2, fill=Colour)
+#Pengembangan Kode Soal 1 menjadi Kode Soal 3
+class Controller(object):
+  def __init__(self):
+    self.model = Model()
+    self.view = View()
 
-drawing_frame = Frame(root)
-drawing_frame.grid(row=0, column=0, pady=10, padx=10, sticky="ew")
-root.grid_columnconfigure(0, weight=1)
+  def get_services(self):
+    services = self.model.services.keys()
+    return(self.view.list_services(services))
 
-# Label
-instruction_label = Label(drawing_frame, text="Klik dan Drag untuk Menggambar")
-instruction_label.pack()
+  def get_pricing(self):
+    services = self.model.services.keys()
+    return(self.view.list_pricing(services))
 
-# Canvas
-w = Canvas(drawing_frame, width=400, height=250, highlightthickness=1,
-           highlightbackground="black", bg="white")
-w.pack(pady=5)
-w.bind("<B1-Motion>", paint)
+  def bid_price(self):
+    tawar = input("What service do you want to bid? email, sms, or voice : ")
+    harga = int(input("Enter the price you want (in $) : "))
+    Model.services[tawar]['price'] = harga
+    print("Price according to your bid: ")
+    self.get_pricing()
 
-# fungsi hapus entry
-def hapus():
-    entry.delete(0, END)
-
-# ---  Text, Entry, dan Button ---
-jlabel = Label(root, text="Nama:")
-jlabel.grid(row=2, column=0, sticky="w", padx=45, pady=(0, 10))
-nama_text = Text(root, height = 1, width = 6)
-nama_text.grid(row=2, column=0, padx=0, pady=(0, 10))
-reset_button = Button(root, text="Reset Entry", command=hapus)
-reset_button.grid(row=3, column=0, sticky="w", padx=45, pady=(0, 10))
-entry = Entry(root, width=30)
-entry.grid(row=3, column=0, padx=45, pady=(0, 10))
-
-mainloop()
+#Instansiasi objek
+controller = Controller()
+print("Services Provided:")
+controller.get_services()
+print("Pricing for Services:")
+controller.get_pricing()
+controller.bid_price()
